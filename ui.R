339f887013ae26b78464df85ade8dc2da5806f75
dutri001 <loic.dutrieux@gmail.com>
2014-08-21 01:26:42 +0200

    Added disclamer on work in progress to the UI

--- ui.R
+++ ui.R
@@ -36,6 +36,7 @@ shinyUI(fluidPage(
       
     ),
     
-    mainPanel(plotOutput("bfmPlot"))
+    mainPanel(plotOutput("bfmPlot"),
+              p('This is a work in progress version;', span('Time series number', style = 'color:green'), 'is a set of 300 random NDVI time-series selected from a Landsat stack covering a study area in Bolivia; Future version will include an upload field for users to upload their own time-series objects. Utilities to easily generate and format these objects will be added to the bfastSpatial package. I am also planning to improve the ggploting and add the possibility to select a date range for the selection of the history period.'))
   )
 ))
